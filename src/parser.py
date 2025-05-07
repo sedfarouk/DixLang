3feb038dc0a95f02db141eb4577248ccfcd2dc14
sedfarouk <sadikalhanssah@gmail.com>
2025-05-07 11:46:52 +0000

    refactor: edit parser to follow grammar.txt

--- src/parser.py
+++ src/parser.py
@@ -64,19 +64,12 @@ class Parser:
         if not res.error and self.current_tok.type != TT_EOF:
             return res.failure(InvalidSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end, "'Expected '+', '-', '*', '^' or '/'"))
         return res
-
-    def factor(self):
+    
+    def atom(self):
         res = ParseResult()
         tok = self.current_tok
 
-        if tok.type in (TT_PLUS, TT_MINUS):
-            res.register(self.advance())
-            factor = res.register(self.factor())
-
-            if res.error: return res
-            return res.success(UnaryOpNode(tok, factor))
-
-        elif tok.type in (TT_INT, TT_FLOAT):
+        if tok.type in (TT_INT, TT_FLOAT):
             res.register(self.advance())
             return res.success(NumberNode(tok))
 
@@ -91,23 +84,42 @@ class Parser:
             else:
                 return res.failure(InvalidSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end, "Expected ')'"))
         
-        return res.failure(InvalidSyntaxError(tok.pos_start, tok.pos_end, 'Expected int or float'))
+        return res.failure(InvalidSyntaxError(tok.pos_start, tok.pos_end, "Expected int, float, '+', '-' or '("))
+
+    def power(self):
+        return self.bin_op(self.atom, (TT_POW, ), self.factor)
+
+    def factor(self):
+        res = ParseResult()
+        tok = self.current_tok
+
+        if tok.type in (TT_PLUS, TT_MINUS):
+            res.register(self.advance())
+            factor = res.register(self.factor())
+
+            if res.error: return res
+            return res.success(UnaryOpNode(tok, factor))
+        
+        return self.power()
 
     def term(self):
-        return self.bin_op(self.factor, (TT_MUL, TT_DIV, TT_POW))
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
 
     def expr(self):
         return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
 
-    def bin_op(self, func, ops):
+    def bin_op(self, func_a, ops, func_b=None):
+        if func_b == None:
+            func_b = func_a
+
         res = ParseResult()
-        left = res.register(func()) 
+        left = res.register(func_a()) 
         if res.error: return res
 
         while self.current_tok.type in ops:
             op_tok = self.current_tok
             res.register(self.advance())
-            right = res.register(func())
+            right = res.register(func_b())
 
             if res.error: 
                 return res
